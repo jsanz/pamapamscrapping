5ad2e1ae49712b3e00876ab973f816bebd3b0cac
Jorge Sanz <jorge.sanz@elastic.co>
2019-12-16 10:42:42 +0100

    first week work

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # pamapamscrapping
+
 Small scrapper for Pam a Pam project
+
+## Set up
+
+This is a nodejs project, just use your favorite package manager to install and run the script to generate the output. 
+
+```
+$ yarn install
+```
+
+
+## Run
+
+A `.cache` folder will be created to store the intermediate assets and you can use the flag `-f` to ensure they are refreshed.
+
+```
+$ yarn start --help
+yarn run v1.21.1
+$ ts-node src/index.ts --help
+Options:
+  --help       Show help                                               [boolean]
+  --version    Show version number                                     [boolean]
+  -f, --force  Force to download the assets                            [boolean]
+Done in 1.48s.
+```
+
+**Note**: If you set up the environment variable `DEBUG=pamapam` you will get further details on the process being executed.
+
+## TO DO
+
+Finalize the exporting process:
+
+- [ ] Set up a file output option (mandatory)
+- [ ] Reorganize a bit the script to separate TypeScript types and a main function outside the initial command line interface script
+- [ ] Expose a simple web interface to generate and download the CSV
+- [ ] Try to deploy the script to a PaaS like `now` or `heroku`
---
+++ src/index.ts
@@ -0,0 +1,175 @@
+const debug = require('debug')('pamapam');
+import * as fs from 'fs';
+import * as path from 'path';
+import { default as cheerio } from 'cheerio';
+
+import { default as rp } from 'request-promise';
+
+import { parse }from 'json2csv';
+
+const args = require('yargs')
+    .alias('f', 'force')
+    .boolean('f')
+    .describe('f', 'Force to download the assets').argv;
+
+const FORCE = args.force;
+const CACHE_DIR = path.join(process.cwd(), '.cache');
+
+async function downloadAsset(url: URL, path: fs.PathLike) {
+    if (!fs.existsSync(path) || FORCE) {
+        debug('Downloading resource...');
+        const resource = await rp(url.href);
+        debug('Saving it into disk', path);
+        fs.writeFileSync(path, resource);
+    }
+    return fs.readFileSync(path);
+}
+
+type Chincheta = {
+    id: string;
+    title: string;
+    latitude: number;
+    longitude: number;
+    sector: string;
+    url: string;
+};
+
+type businessPage = { id: string; content: string };
+
+type businessData = {
+    id: string;
+    tagLine: string;
+    criteria: criteriaRating[];
+};
+
+type criteriaRating = {
+    name: string;
+    value: number;
+};
+
+function processChincheta(element: CheerioElement): Chincheta {
+    const urlEl = element.childNodes.filter(el => el.tagName == 'h4');
+    const url = urlEl.length == 1 ? urlEl[0].firstChild.attribs['href'] : '';
+
+    const sectorEls = element.childNodes.filter(el => el.type == 'text');
+    let sector = '';
+    if (sectorEls.length == 1 && 'data' in sectorEls[0]) {
+        if (sectorEls[0].data && typeof sectorEls[0].data === 'string') {
+            sector = sectorEls[0].data.trim();
+        }
+    }
+
+    let id = '';
+    if (url) {
+        const parts = url.split('/').filter(el => el.length != 0);
+        id = parts[parts.length - 1];
+    }
+
+    return {
+        id,
+        title: element.attribs['data-markertitle'],
+        latitude: parseFloat(element.attribs['data-markerlat']),
+        longitude: parseFloat(element.attribs['data-markerlon']),
+        sector,
+        url,
+    };
+}
+
+function getPoints($: CheerioStatic): Chincheta[] {
+    debug('Getting points');
+    const divs = $('.wpv-addon-maps-marker') as Cheerio;
+    if (divs.length == 0) {
+        throw new Error('Main DIV not found');
+    }
+    const results: Chincheta[] = [];
+    divs.each((_, element) => {
+        results.push(processChincheta(element));
+    });
+    return results;
+}
+
+function getTagLine($: CheerioStatic): string {
+    return $('.container > .row > .col-sm-6 > p')
+        .first()
+        .text();
+}
+
+function getCriteria($: CheerioStatic) {
+    const criteria: criteriaRating[] = [];
+    $('#criteris > li').each((_, el) => {
+        
+        const names = el.children.filter(e => e.type == 'text' && e.data && e.data.trim() != '');
+        let name = '';
+
+        if (names[0] && names[0].data) {
+            name = names[0].data.trim();
+        }
+
+        const value = $(el)
+            .children('span.badge')
+            .children('span')
+            .children('span.dashicons-star-filled').length;
+
+        criteria.push({
+            name,
+            value,
+        });
+    });
+
+    return criteria;
+}
+
+function getBusinessData({ id, content }: businessPage): businessData {
+    const $ = cheerio.load(content);
+    const tagLine = getTagLine($);
+    const criteria = getCriteria($);
+
+    return {
+        id,
+        tagLine,
+        criteria,
+    } as businessData;
+}
+
+async function downloadBusinessPage(chincheta: Chincheta): Promise<businessPage> {
+    const cPath = path.join(CACHE_DIR, chincheta.id + 'html');
+    const content = await downloadAsset(new URL(chincheta.url), cPath);
+    return {
+        ...chincheta,
+        content: content.toString(),
+    };
+}
+
+(async () => {
+    // make the cache dir if it not exists
+    if (!fs.existsSync(CACHE_DIR)) {
+        console.log('Creating the cache directory ".cache"');
+        fs.mkdirSync(CACHE_DIR);
+    }
+
+    const rootPath = path.join(CACHE_DIR, 'index.html');
+    const rootUrl = new URL('https://pamapampv.org/directori-de-punts/');
+
+    const rootContent = await downloadAsset(rootUrl, rootPath);
+    const $ = cheerio.load(rootContent);
+    const chinchetas = getPoints($);
+
+    const businessHtml = await chinchetas.map(downloadBusinessPage);
+
+    const extendedPointsPromises = await businessHtml.map(async (business: Promise<businessPage>) => {
+        const b = await business;
+        const extendeData = { ...b, ...getBusinessData(b) };
+        delete extendeData.content;
+        return extendeData;
+    });
+
+    Promise.all(extendedPointsPromises).then(results => {
+        const pointsPath = path.join(CACHE_DIR, 'points.json');
+        fs.writeFileSync(pointsPath, JSON.stringify(results));
+        console.log(`File gererated: ${pointsPath}`);
+
+        const fields = ['title','longitude','latitude','sector','url'];
+        const opts = {fields};
+        console.log(parse(results,opts))
+    });
+})();
